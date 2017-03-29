17766fa764247e50cffdf56e0cc114c7316e08e5
mrtcndkn <mrtcndkn@fd9223c1-c77d-41e1-af93-17165896985c>
2017-03-29 15:47:22 +0000

    cosmetics

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -6,9 +6,9 @@ import {Component} from '@angular/core';
         <div class="footer clearfix">
             <img src="../assets/layout/images/logo-white.png"/>
             <span>PrimeNG ATLANTIS</span>
-            <a href="https://github.com/primefaces"><i class="fa fa-github"></i></a>
+            <a href="https://github.com/primefaces/primeng"><i class="fa fa-github"></i></a>
             <a href="https://www.facebook.com/groups/primefaces"><i class="fa fa-facebook"></i></a>
-            <a href="http://twitter.com/primefaces"><i class="fa fa-twitter"></i></a>
+            <a href="https://twitter.com/prime_ng"><i class="fa fa-twitter"></i></a>
         </div>
     `
 })
