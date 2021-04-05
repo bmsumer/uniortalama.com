6a4cfe8d57fa0aa4c79b184bd39e78d67791aa1c
Merve Özçifçi <merveozcifci7@gmail.com>
2021-04-05 14:06:05 +0300

    last dashboard widget

--- src/app/demo/view/dashboarddemo.component.ts
+++ src/app/demo/view/dashboarddemo.component.ts
@@ -33,6 +33,26 @@ export class DashboardDemoComponent implements OnInit {
 
     customerChartOptions: any;
 
+    carouselResponsiveOptions: any[] = [
+        {
+            breakpoint: '1024px',
+            numVisible: 3,
+            numScroll: 3
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 2,
+            numScroll: 2
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1,
+            numScroll: 1
+        }
+    ];
+
+    customer: any[];
+
     constructor(private customerService: CustomerService, private eventService: EventService,
                 private breadcrumbService: AppBreadcrumbService) {
         this.breadcrumbService.setItems([
@@ -334,5 +354,20 @@ export class DashboardDemoComponent implements OnInit {
                 }]
             },
         };
+
+        this.customer = [
+            {user: '9,673 Users', value: '$8,362,478', image: 'nasa'},
+            {user: '9,395 Users', value: '$7,927,105', image: 'beats'},
+            {user: '7,813 Users', value: '$6,471,594', image: 'go-pro'},
+            {user: '7,613 Users', value: '$5,697,883', image: 'north-face'},
+            {user: '98,673 Users', value: '$7,653,311', image: 'mc'},
+            {user: '5,645 Users', value: '$4,567,823', image: 'dell'},
+            {user: '5,153 Users', value: '$5,342,678', image: 'wwf'},
+            {user: '4,338 Users', value: '$5,867,391', image: 'bmw'},
+            {user: '4,170 Users', value: '$4,647,233', image: 'pepsi'},
+            {user: '3,765 Users', value: '$4,123,876', image: 'huawei'},
+            {user: '3,490 Users', value: '$3,688,362', image: 'dyson'},
+            {user: '2,976 Users', value: '$3,978,478', image: 'p&g'},
+        ];
     }
 }
