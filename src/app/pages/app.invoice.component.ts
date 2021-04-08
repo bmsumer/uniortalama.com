475ccdc5f4cf922acefb951d7200ce76981c4504
Merve Özçifçi <merveozcifci7@gmail.com>
2021-04-08 14:58:48 +0300

    invoice added

--- src/app/pages/app.invoice.component.ts
+++ src/app/pages/app.invoice.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {AppBreadcrumbService} from '../app.breadcrumb.service';
+import {AppComponent} from '../app.component';
 
 @Component({
     templateUrl: './app.invoice.component.html'
 })
 export class AppInvoiceComponent {
 
-    constructor(private breadcrumbService: AppBreadcrumbService) {
+    constructor(private breadcrumbService: AppBreadcrumbService, public app: AppComponent) {
         this.breadcrumbService.setItems([
             { label: 'Pages' },
             { label: 'Invoice', routerLink: ['/pages/invoice'] }
