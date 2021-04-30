89a893ca45e60e54556b7d663843a0317e471b24
Merve Özçifçi <merveozcifci7@gmail.com>
2021-04-30 18:03:11 +0300

    carousel logos and landing button changed

--- src/app/demo/view/dashboarddemo.component.ts
+++ src/app/demo/view/dashboarddemo.component.ts
@@ -358,16 +358,16 @@ export class DashboardDemoComponent implements OnInit {
         this.customer = [
             {user: '9,673 Users', value: '$8,362,478', image: 'nasa'},
             {user: '9,395 Users', value: '$7,927,105', image: 'beats'},
-            {user: '7,813 Users', value: '$6,471,594', image: 'go-pro'},
-            {user: '7,613 Users', value: '$5,697,883', image: 'north-face'},
+            {user: '7,813 Users', value: '$6,471,594', image: 'gopro'},
+            {user: '7,613 Users', value: '$5,697,883', image: 'north'},
             {user: '98,673 Users', value: '$7,653,311', image: 'mc'},
             {user: '5,645 Users', value: '$4,567,823', image: 'dell'},
             {user: '5,153 Users', value: '$5,342,678', image: 'wwf'},
             {user: '4,338 Users', value: '$5,867,391', image: 'bmw'},
             {user: '4,170 Users', value: '$4,647,233', image: 'pepsi'},
-            {user: '3,765 Users', value: '$4,123,876', image: 'huawei'},
-            {user: '3,490 Users', value: '$3,688,362', image: 'dyson'},
-            {user: '2,976 Users', value: '$3,978,478', image: 'p&g'},
+            {user: '3,765 Users', value: '$4,123,876', image: 'netflix'},
+            {user: '3,490 Users', value: '$3,688,362', image: 'deloitte'},
+            {user: '2,976 Users', value: '$3,978,478', image: 'pg'},
         ];
     }
 }
