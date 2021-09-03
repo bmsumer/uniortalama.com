b02ce4bf7ac41a191ee436fb840bce506a841b7c
Merve Özçifçi <merveozcifci7@gmail.com>
2021-09-03 11:40:54 +0300

    charts updated

--- src/app/demo/view/chartsdemo.component.ts
+++ src/app/demo/view/chartsdemo.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {AppBreadcrumbService} from '../../app.breadcrumb.service';
 
 @Component({
     templateUrl: './chartsdemo.component.html'
@@ -8,26 +7,23 @@ export class ChartsDemoComponent implements OnInit {
 
     lineData: any;
 
-    lineBarOptions: any;
-
     barData: any;
 
     pieData: any;
-
-    pieDoughnutOptions: any;
 
     polarData: any;
 
     radarData: any;
 
-    radarPolarOptions: any;
+    lineOptions: any;
 
-    constructor(private breadcrumbService: AppBreadcrumbService) {
-        this.breadcrumbService.setItems([
-            { label: 'UI Kit' },
-            { label: 'Chart', routerLink: ['/uikit/charts'] }
-        ]);
-    }
+    barOptions: any;
+
+    pieOptions: any;
+
+    polarOptions: any;
+
+    radarOptions: any;
 
     ngOnInit() {
         this.lineData = {
@@ -38,16 +34,46 @@ export class ChartsDemoComponent implements OnInit {
                     data: [65, 59, 80, 81, 56, 55, 40],
                     fill: false,
                     backgroundColor: 'rgb(255, 205, 86)',
-                    borderColor: 'rgb(255, 205, 86)'
+                    borderColor: 'rgb(255, 205, 86)',
+                    tension: .4
                 },
                 {
                     label: 'Second Dataset',
                     data: [28, 48, 40, 19, 86, 27, 90],
                     fill: false,
                     backgroundColor: 'rgb(75, 192, 192)',
-                    borderColor: 'rgb(75, 192, 192)'
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: .4
                 }
             ]
+        };
+
+        this.lineOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        fontColor: '#A0A7B5'
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        fontColor: '#A0A7B5'
+                    },
+                    grid: {
+                        color:  'rgba(160, 167, 181, .3)',
+                    }
+                },
+                y: {
+                    ticks: {
+                        fontColor: '#A0A7B5'
+                    },
+                    grid: {
+                        color:  'rgba(160, 167, 181, .3)',
+                    }
+                },
+            }
         };
 
         this.barData = {
@@ -68,30 +94,31 @@ export class ChartsDemoComponent implements OnInit {
             ]
         };
 
-        this.lineBarOptions = {
-            legend: {
-                display: true,
-                labels: {
-                    fontColor: '#A0A7B5'
+        this.barOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        fontColor: '#A0A7B5'
+                    }
                 }
             },
             scales: {
-                yAxes: [{
+                x: {
                     ticks: {
                         fontColor: '#A0A7B5'
                     },
-                    gridLines: {
+                    grid: {
                         color:  'rgba(160, 167, 181, .3)',
                     }
-                }],
-                xAxes: [{
+                },
+                y: {
                     ticks: {
                         fontColor: '#A0A7B5'
                     },
-                    gridLines: {
+                    grid: {
                         color:  'rgba(160, 167, 181, .3)',
                     }
-                }],
+                },
             }
         };
 
@@ -105,16 +132,57 @@ export class ChartsDemoComponent implements OnInit {
                         'rgb(255, 99, 132)',
                         'rgb(255, 205, 86)',
                         'rgb(75, 192, 192)'
-                    ],
-                    borderColor: '#A0A7B5'
+                    ]
                 }]
         };
 
-        this.pieDoughnutOptions = {
-            legend: {
-                display: true,
-                labels: {
-                    fontColor: '#A0A7B5'
+        this.pieOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        fontColor: '#A0A7B5'
+                    }
+                }
+            }
+        };
+
+        this.polarData = {
+            datasets: [{
+                data: [
+                    11,
+                    16,
+                    7,
+                    3
+                ],
+                backgroundColor: [
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)'
+                ],
+                label: 'My dataset'
+            }],
+            labels: [
+                'Blue',
+                'Purple',
+                'Orange',
+                'Green'
+            ]
+        };
+
+        this.polarOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        fontColor: '#A0A7B5'
+                    }
+                }
+            },
+            scales: {
+                r: {
+                    grid: {
+                        color: 'rgba(160, 167, 181, .3)'
+                    }
                 }
             }
         };
@@ -145,45 +213,21 @@ export class ChartsDemoComponent implements OnInit {
             ]
         };
 
-        this.polarData = {
-            datasets: [{
-                data: [
-                    11,
-                    16,
-                    7,
-                    3
-                ],
-                backgroundColor: [
-                    'rgb(54, 162, 235)',
-                    'rgb(255, 99, 132)',
-                    'rgb(255, 205, 86)',
-                    'rgb(75, 192, 192)'
-                ],
-                label: 'My dataset'
-            }],
-            labels: [
-                'Blue',
-                'Purple',
-                'Orange',
-                'Green'
-            ]
-        };
-
-        this.radarPolarOptions = {
-            legend: {
-                display: true,
-                labels: {
-                    fontColor: '#A0A7B5'
+        this.radarOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        fontColor: '#A0A7B5'
+                    }
                 }
             },
-            scale: {
-                gridLines: {
-                    color: '#A0A7B5'
-                },
-                ticks: {
-                    backdropColor: 'transparent'
+            scales: {
+                r: {
+                    grid: {
+                        color: 'rgba(160, 167, 181, .3)'
+                    }
                 }
-            },
+            }
         };
     }
 }
--- src/app/utilities/widgets.component.ts
+++ src/app/utilities/widgets.component.ts
@@ -72,30 +72,30 @@ export class WidgetsComponent implements OnInit{
         };
 
         this.visitorChartOptions = {
-            legend: {
-                position: 'top',
-                align: 'end'
+            plugins: {
+                legend: {
+                    position: 'top',
+                    align: 'end'
+                }
             },
             responsive: true,
             hover: {
                 mode: 'index'
             },
             scales: {
-                yAxes: [{
-                    ticks: {
-                        min: 500,
-                        max: 900
-                    },
-                    gridLines: {
+                y: {
+                    min: 500,
+                    max: 900,
+                    grid: {
                         display: false
                     }
-                }],
-                xAxes: [{
+                },
+                x: {
                     barPercentage: 0.5,
-                    gridLines: {
+                    grid: {
                         display: false
                     }
-                }]
+                }
             }
         };
 
@@ -108,7 +108,8 @@ export class WidgetsComponent implements OnInit{
                     borderColor: '#EEE500',
                     pointBackgroundColor: '#EEE500',
                     backgroundColor: 'rgba(238, 229, 0, 0.05)',
-                    fill: true
+                    fill: true,
+                    tension: .4
                 },
                 {
                     label: 'Revenue',
@@ -117,6 +118,7 @@ export class WidgetsComponent implements OnInit{
                     pointBackgroundColor: '#00D0DE',
                     backgroundColor: 'rgba(0, 208, 222, 0.05)',
                     fill: true,
+                    tension: .4
                 },
                 {
                     label: 'Expenses',
@@ -124,7 +126,8 @@ export class WidgetsComponent implements OnInit{
                     borderColor: '#FC6161',
                     pointBackgroundColor: '#FC6161',
                     backgroundColor: 'rgba(253, 72, 74, 0.05)',
-                    fill: true
+                    fill: true,
+                    tension: .4
                 },
                 {
                     label: 'Customer',
@@ -132,7 +135,8 @@ export class WidgetsComponent implements OnInit{
                     borderColor: '#0F8BFD',
                     pointBackgroundColor: '#0F8BFD',
                     backgroundColor: 'rgba(15, 139, 253, 0.05)',
-                    fill: true
+                    fill: true,
+                    tension: .4
                 }]
         };
 
@@ -142,13 +146,11 @@ export class WidgetsComponent implements OnInit{
                 mode: 'index'
             },
             scales: {
-                yAxes: [{
-                    ticks: {
-                        min: 0,
-                        max: 60,
-                        stepSize: 5
-                    }
-                }]
+                y: {
+                    min: 0,
+                    max: 60,
+                    stepSize: 5
+                }
             }
         };
 
@@ -159,128 +161,164 @@ export class WidgetsComponent implements OnInit{
                     data: [10, 25, 48, 35, 54, 70],
                     backgroundColor: '#AAABDD',
                     hoverBackgroundColor: '#AAABDD',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [18, 35, 23, 30, 59, 65],
                     backgroundColor: '#A0A0D9',
                     hoverBackgroundColor: '#A0A0D9',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [20, 47, 46, 46, 61, 70],
                     backgroundColor: '#ACADDE',
                     hoverBackgroundColor: '#ACADDE',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [17, 34, 18, 48, 67, 68],
                     backgroundColor: '#ABABDD',
                     hoverBackgroundColor: '#ABABDD',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [9, 37, 47, 50, 60, 62],
                     backgroundColor: '#A2A3D9',
                     hoverBackgroundColor: '#A2A3D9',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [8, 48, 40, 52, 72, 75],
                     backgroundColor: '#A3A4DA',
                     hoverBackgroundColor: '#A3A4DA',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [10, 18, 50, 47, 63, 80],
                     backgroundColor: '#A2A3D9',
                     hoverBackgroundColor: '#A2A3D9',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [20, 36, 39, 58, 59, 85],
                     backgroundColor: '#8485CD',
                     hoverBackgroundColor: '#8485CD',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [30, 45, 35, 50, 54, 81],
                     backgroundColor: '#7D7ECA',
                     hoverBackgroundColor: '#7D7ECA',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [28, 35, 52, 56, 60, 77],
                     backgroundColor: '#8384CD',
                     hoverBackgroundColor: '#8384CD',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [40, 40, 38, 45, 68, 86],
                     backgroundColor: '#8F90D2',
                     hoverBackgroundColor: '#8F90D2',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [50, 23, 27, 34, 65, 90],
                     backgroundColor: '#8C8DD0',
                     hoverBackgroundColor: '#8C8DD0',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [29, 27, 29, 42, 55, 84],
                     backgroundColor: '#9495D4',
                     hoverBackgroundColor: '#9495D4',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [10, 37, 47, 29, 59, 80],
                     backgroundColor: '#9696D4',
                     hoverBackgroundColor: '#9696D4',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [10, 54, 42, 38, 63, 83],
                     backgroundColor: '#7273C6',
                     hoverBackgroundColor: '#7273C6',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [25, 44, 50, 56, 65, 92],
                     backgroundColor: '#5F60BE',
                     hoverBackgroundColor: '#5F60BE',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [30, 43, 48, 45, 73, 78],
                     backgroundColor: '#5C5DBD',
                     hoverBackgroundColor: '#5C5DBD',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 },
                 {
                     data: [29, 47, 54, 60, 77, 86],
                     backgroundColor: '#5C5DBD',
                     hoverBackgroundColor: '#5C5DBD',
-                    fill: true
+                    fill: true,
+                    categoryPercentage: 1.0,
+                    barPercentage: 1.0
                 }
             ]
         };
 
         this.customerChartOptions = {
-            legend: {
-                display: false
+            plugins: {
+                legend: {
+                    display: false
+                }
             },
             scales: {
-                yAxes: [{
+                y: {
                     display: false,
-                }],
-                xAxes: [{
-                    gridLines : {
+                },
+                x: {
+                    grid : {
                         display : false
-                    },
-                    categoryPercentage: 1.0,
-                    barPercentage: 1.0
-                }]
+                    }
+                }
             },
         };
     }
