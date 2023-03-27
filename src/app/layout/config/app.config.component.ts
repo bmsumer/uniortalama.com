aece9db5133a564c8440ca964cf7d652eeeb178e
ATAKAN TEPE <tepe_atakan@outlook.com>
2023-03-27 13:31:57 +0300

    update files

--- src/app/layout/config/app.config.component.ts
+++ src/app/layout/config/app.config.component.ts
@@ -1,22 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MenuService } from '../app.menu.service';
-import { ColorScheme, LayoutService, MenuMode } from '../service/app.layout.service';
+import { Component, Input, OnInit } from "@angular/core";
+import { MenuService } from "../app.menu.service";
+import {
+    ColorScheme,
+    LayoutService,
+    MenuMode,
+} from "../service/app.layout.service";
 
 @Component({
-    selector: 'app-config',
-    templateUrl: './app.config.component.html'
+    selector: "app-config",
+    templateUrl: "./app.config.component.html",
 })
 export class AppConfigComponent implements OnInit {
-
     @Input() minimal: boolean = false;
 
     componentThemes: any[] = [];
 
     layoutThemes: any[] = [];
 
-    scales: number[] = [12,13,14,15,16];
+    scales: number[] = [12, 13, 14, 15, 16];
 
-    constructor(public layoutService: LayoutService, public menuService: MenuService) { }
+    constructor(
+        public layoutService: LayoutService,
+        public menuService: MenuService
+    ) {}
 
     get visible(): boolean {
         return this.layoutService.state.configSidebarVisible;
@@ -40,7 +46,11 @@ export class AppConfigComponent implements OnInit {
 
     set menuMode(_val: MenuMode) {
         this.layoutService.config.menuMode = _val;
-        if (this.layoutService.isSlimPlus() || this.layoutService.isSlim() || this.layoutService.isHorizontal()) {
+        if (
+            this.layoutService.isSlimPlus() ||
+            this.layoutService.isSlim() ||
+            this.layoutService.isHorizontal()
+        ) {
             this.menuService.reset();
         }
     }
@@ -69,18 +79,16 @@ export class AppConfigComponent implements OnInit {
         this.layoutService.config.ripple = _val;
     }
 
-
-
     ngOnInit() {
         this.componentThemes = [
-            { name: 'blue', color: '#0F8BFD' },
-            { name: 'green', color: '#0BD18A' },
-            { name: 'magenta', color: '#EC4DBC' },
-            { name: 'orange', color: '#FD9214' },
-            { name: 'purple', color: '#873EFE' },
-            { name: 'red', color: '#FC6161' },
-            { name: 'teal', color: '#00D0DE' },
-            { name: 'yellow', color: '#EEE500' }
+            { name: "blue", color: "#0F8BFD" },
+            { name: "green", color: "#0BD18A" },
+            { name: "magenta", color: "#EC4DBC" },
+            { name: "orange", color: "#FD9214" },
+            { name: "purple", color: "#873EFE" },
+            { name: "red", color: "#FC6161" },
+            { name: "teal", color: "#00D0DE" },
+            { name: "yellow", color: "#EEE500" },
         ];
     }
 
@@ -89,42 +97,62 @@ export class AppConfigComponent implements OnInit {
     }
 
     changeColorScheme(colorScheme: ColorScheme) {
-        const themeLink = <HTMLLinkElement>document.getElementById('theme-link');
-        const themeLinkHref = themeLink.getAttribute('href');
-        const currentColorScheme = 'theme-' + this.layoutService.config.colorScheme;
-        const newColorScheme = 'theme-' + colorScheme;
-        const newHref = themeLinkHref!.replace(currentColorScheme, newColorScheme);
-        this.replaceThemeLink(newHref, () => {
-            this.layoutService.config.colorScheme = colorScheme;
-            this.layoutService.onConfigUpdate();
-        },'theme-link');
+        const themeLink = <HTMLLinkElement>(
+            document.getElementById("theme-link")
+        );
+        const themeLinkHref = themeLink.getAttribute("href");
+        const currentColorScheme =
+            "theme-" + this.layoutService.config.colorScheme;
+        const newColorScheme = "theme-" + colorScheme;
+        const newHref = themeLinkHref!.replace(
+            currentColorScheme,
+            newColorScheme
+        );
+        this.replaceThemeLink(
+            newHref,
+            () => {
+                this.layoutService.config.colorScheme = colorScheme;
+                this.layoutService.onConfigUpdate();
+            },
+            "theme-link"
+        );
     }
 
     changeTheme(theme: string) {
-        const themeLink = <HTMLLinkElement>document.getElementById('theme-link');
- 
-        const newHref = themeLink.getAttribute('href')!.replace(this.layoutService.config.theme, theme);
-        this.replaceThemeLink(newHref, () => {
-            this.layoutService.config.theme = theme;
-            this.layoutService.onConfigUpdate();
-        },'theme-link');
+        const themeLink = <HTMLLinkElement>(
+            document.getElementById("theme-link")
+        );
+
+        const newHref = themeLink
+            .getAttribute("href")!
+            .replace(this.layoutService.config.theme, theme);
+        this.replaceThemeLink(
+            newHref,
+            () => {
+                this.layoutService.config.theme = theme;
+                this.layoutService.onConfigUpdate();
+            },
+            "theme-link"
+        );
     }
 
-
-    replaceThemeLink(href: string, onComplete: Function,linkId:string) {
+    replaceThemeLink(href: string, onComplete: Function, linkId: string) {
         const id = linkId;
-        console.log(linkId)
+        console.log(linkId);
         const themeLink = <HTMLLinkElement>document.getElementById(id);
-        const cloneLinkElement = <HTMLLinkElement>themeLink.cloneNode(true);
+        const cloneLinkElement = <HTMLLinkElement>themeLink.cloneNode(true);                                
 
-        cloneLinkElement.setAttribute('href', href);
-        cloneLinkElement.setAttribute('id', id + '-clone');
+        cloneLinkElement.setAttribute("href", href);
+        cloneLinkElement.setAttribute("id", id + "-clone");
 
-        themeLink.parentNode!.insertBefore(cloneLinkElement, themeLink.nextSibling);
+        themeLink.parentNode!.insertBefore(
+            cloneLinkElement,
+            themeLink.nextSibling
+        );
 
-        cloneLinkElement.addEventListener('load', () => {
+        cloneLinkElement.addEventListener("load", () => {
             themeLink.remove();
-            cloneLinkElement.setAttribute('id', id);
+            cloneLinkElement.setAttribute("id", id);
             onComplete();
         });
     }
@@ -140,7 +168,6 @@ export class AppConfigComponent implements OnInit {
     }
 
     applyScale() {
-        document.documentElement.style.fontSize = this.scale + 'px';
+        document.documentElement.style.fontSize = this.scale + "px";
     }
-    
 }
